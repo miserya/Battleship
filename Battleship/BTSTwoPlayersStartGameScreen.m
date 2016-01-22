528810d532d0226c693c4647404e11802678d361
Mariya Golubeva <mariya@Mariyas-MacBook-Pro.local>
2016-01-22 17:55:50 +0200

    bugs fixing

--- Battleship/BTSTwoPlayersStartGameScreen.m
+++ Battleship/BTSTwoPlayersStartGameScreen.m
@@ -21,9 +21,15 @@ typedef enum {
 @property (weak, nonatomic) IBOutlet UIImageView *player1_Ship;
 @property (weak, nonatomic) IBOutlet UIImageView *player2_Ship;
 @property (nonatomic) BTSPlayer currentPlayer;
+@property (nonatomic) BOOL isGameInitialized;
 
 @property (nonatomic, strong) BTSGameField *tapped_gameFieldPlayer1;
 @property (nonatomic, strong) BTSGameField *tapped_gameFieldPlayer2;
+
+@property (nonatomic, copy) void (^animateShip1)();
+@property (nonatomic, copy) void (^animateShip2)();
+@property (nonatomic, copy) BOOL (^canAnimateShip1)();
+@property (nonatomic, copy) BOOL (^canAnimateShip2)();
 
 @end
 
@@ -31,14 +37,30 @@ typedef enum {
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.isGameInitialized = NO;
     
-    self.tapped_gameFieldPlayer1 = [BTSGameField new];
-    self.player1_GameField = [BTSGameField new];
-    [self.player1_GameField generate];
+//    __block BTSTwoPlayersStartGameScreen *wSelf = self;
     
-    self.tapped_gameFieldPlayer2 = [BTSGameField new];
-    self.player2_GameField = [BTSGameField new];
-    [self.player2_GameField generate];
+    self.canAnimateShip1 = ^BOOL() {
+        return (self.currentPlayer != BTSPlayer_1);
+    };
+    self.canAnimateShip2 = ^BOOL() {
+        return (self.currentPlayer != BTSPlayer_2);
+    };
+    self.animateShip1 = ^void() {
+        if (YES && self.canAnimateShip2()) {
+            [UIView animateWithDuration:0.5f delay:0.f options:UIViewAnimationOptionCurveLinear|UIViewAnimationOptionRepeat|UIViewAnimationOptionAutoreverse animations:^{
+                self.player2_Ship.alpha = 0.2f;
+            } completion:nil];
+        }
+    };
+    self.animateShip2 = ^void() {
+        if (YES && self.canAnimateShip1()) {
+            [UIView animateWithDuration:0.5f delay:0.f options:UIViewAnimationOptionCurveLinear|UIViewAnimationOptionRepeat|UIViewAnimationOptionAutoreverse animations:^{
+                self.player1_Ship.alpha = 0.2f;
+            } completion:nil];
+        }
+    };
 }
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
@@ -46,28 +68,8 @@ typedef enum {
     self.player1_Ship.alpha = 1.f;
     self.player2_Ship.alpha = 1.f;
     
-    __block __weak BTSTwoPlayersStartGameScreen *wSelf = self;
-    void (^animateShip1)() = ^void() {
-        [UIView animateWithDuration:0.5f delay:0.f options:UIViewAnimationOptionCurveLinear|UIViewAnimationOptionRepeat|UIViewAnimationOptionAutoreverse animations:^{
-            wSelf.player2_Ship.alpha = 0.2f;
-        } completion:nil];
-    };
-    void (^animateShip2)() = ^void() {
-        [UIView animateWithDuration:0.5f delay:0.f options:UIViewAnimationOptionCurveLinear|UIViewAnimationOptionRepeat|UIViewAnimationOptionAutoreverse animations:^{
-            wSelf.player1_Ship.alpha = 0.2f;
-        } completion:nil];
-    };
-    
-    if (self.currentPlayer == BTSPlayer_None) {
-        animateShip1();
-        animateShip2();
-    }
-    else if (self.currentPlayer == BTSPlayer_1) {
-        animateShip1();
-    }
-    else if (self.currentPlayer == BTSPlayer_2) {
-        animateShip2();
-    }
+    self.animateShip1();
+    self.animateShip2();
 }
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
@@ -86,16 +88,29 @@ typedef enum {
         ((BTSGameScreen*)segue.destinationViewController).tapped_gameFieldPlayer2 = self.tapped_gameFieldPlayer2;
     }
 }
-
+- (void)initializeGame {
+    if (!self.isGameInitialized) {
+        self.tapped_gameFieldPlayer1 = [BTSGameField new];
+        self.player1_GameField = [BTSGameField new];
+        [self.player1_GameField generate];
+        
+        self.tapped_gameFieldPlayer2 = [BTSGameField new];
+        self.player2_GameField = [BTSGameField new];
+        [self.player2_GameField generate];
+        self.isGameInitialized = YES;
+    }
+}
 - (IBAction)onPlayer1:(id)sender {
     if (self.currentPlayer == BTSPlayer_None || self.currentPlayer == BTSPlayer_2) {
         self.currentPlayer = BTSPlayer_1;
+        [self initializeGame];
         [self performSegueWithIdentifier:@"BTSShowGameScreen" sender:self];
     }
 }
 - (IBAction)onPlayer2:(id)sender {
     if (self.currentPlayer == BTSPlayer_None || self.currentPlayer == BTSPlayer_1) {
         self.currentPlayer = BTSPlayer_2;
+        [self initializeGame];
         [self performSegueWithIdentifier:@"BTSShowGameScreen" sender:self];
     }
 }
